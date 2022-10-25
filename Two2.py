a1f56d8b3f2962601c498863fe7007eb9931821f
Nathan <84240782+nathancarter209@users.noreply.github.com>
2022-10-25 10:47:59 +0530

    Update Two2.py

--- Two2.py
+++ Two2.py
@@ -1,5 +1,5 @@
 #To find the ASCII value of any character with python
-ch = input("Enter character: ")
+ch = input("Enter character : ")
 
 asc = ord(ch)
-print("ASCII code of",ch,"is:", asc)
+print("ASCII code of",ch,"is : ", asc)
